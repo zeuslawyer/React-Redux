fb7374a9de353b91916ee8811d1030f336bdf3ca
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-10-29 12:29:56 -0700

    METHOD2: pass input handler directly as prop to component, and setState().

--- 1_simple-redux-starter/src/components/Search-bar.js
+++ 1_simple-redux-starter/src/components/Search-bar.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 // Class component, not Functional Component
 class SearchBar extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         searchTerm: ''
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ""
+    };
 
-      this.onInputChange = this.onInputChange.bind(this)
-    }
-  
+    //method biding in constructor method
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
   render() {
-    return  <input onChange={this.onInputChange}/>
+      //method 1 - using the onInputChange() method
+    // return <input onChange={this.onInputChange} />;
+    
+    //method 2 - directly passing event handler to onChange property of the component
+    return <input onChange={event => {this.setState({searchTerm:event.target.value})}} />;
   }
 
-  onInputChange(event){
-    event.preventDefault()
-    this.setState({searchTerm: event.target.value})
-    console.log('state :', this.state);
+  onInputChange(event) {
+    event.preventDefault();
+    this.setState({ searchTerm: event.target.value });
+    console.log("state :", this.state);
   }
-
-};
-
+}
 
 export default SearchBar;
 
