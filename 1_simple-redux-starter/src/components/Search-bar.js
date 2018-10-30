4e4a56f2972cda48a8d048e4522dfe1bf87e2a2f
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-10-29 18:37:36 -0700

    switch to method 2. implement CONTROLLED COMPONENT pattern. WORKING

--- 1_simple-redux-starter/src/components/Search-bar.js
+++ 1_simple-redux-starter/src/components/Search-bar.js
@@ -8,24 +8,37 @@ class SearchBar extends Component {
       searchTerm: ""
     };
 
-    //method biding in constructor method
-    this.onInputChange = this.onInputChange.bind(this);
+    //method biding in constructor method - USED IN METHOD 2
+    // this.onInputChange = this.onInputChange.bind(this);
   }
 
   render() {
-      //method 1 - using the onInputChange() method
-    // return <input onChange={this.onInputChange} />;
-    
-    //method 2 - directly passing event handler to onChange property of the component
-    return <input onChange={event => {this.setState({searchTerm:event.target.value})}} />;
+    //method 1 - using the onInputChange() method
+    // return (
+    //     <div>
+    //         <input onKeyDown={this.onInputChange} />
+    //         Input Value is: {this.state.searchTerm}
+    //     </div>
+    // )
+
+    // method 2 - directly passing event handler to onChange property of the component
+    return (
+      <div>
+        <input
+          value={this.state.searchTerm}
+          onChange={event => this.setState({ searchTerm: event.target.value })}
+        />
+        Input Value is: {this.state.searchTerm}
+      </div>
+    );
   }
 
-  onInputChange(event) {
-    event.preventDefault();
-    this.setState({ searchTerm: event.target.value });
-    console.log("state :", this.state);
-  }
+  //NOT USED IN METHOD 2
+  //   onInputChange(event) {
+  //     event.preventDefault();
+  //     this.setState({ searchTerm: event.target.value});
+  //     console.log("state :", this.state);
+  //   }
 }
 
 export default SearchBar;
-
