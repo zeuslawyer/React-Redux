2ee6f5489ffeebc1b0f16aaa0c1f276a88b84793
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-10-29 18:55:15 -0700

    add YT API Key by importing secrets. moved secrets.js to within src. WORKING

--- 1_simple-redux-starter/src/index.js
+++ 1_simple-redux-starter/src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import YTSearch from 'youtube-api-search';
+import {secrets} from './secrets'
 // importing components
 import SearchBar from './components/Search-bar';
 
-const API_KEY = process.env.REACT_APP_YT_API;
 
+
+const API_KEY = secrets.YOUTUBE_API
+
+YTSearch({key: API_KEY, term: 'evan carmichael'}, function(results){
+  console.log(results)
+  console.log(results[0].id.kind.slice(-6))
+});
 
 const App = () => {
   return (
