1d445f36d22f5d230d66507ef46dc0965cb28fd4
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-02 16:02:32 -0700

    same result, but using ARROW function, to avoid use of this. WORKING

--- 1_simple-redux-starter/src/index.js
+++ 1_simple-redux-starter/src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash'
+import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
@@ -8,10 +8,8 @@ import SearchBar from "./components/Search-bar";
 import VideoList from "./components/Video-list";
 import VideoDetail from "./components/Video-detail";
 
-
 const API_KEY = process.env.REACT_APP_API_KEY;
-const DEFAULT_SEARCH_TERM = 'game of thrones'
-
+const DEFAULT_SEARCH_TERM = "game of thrones";
 
 class App extends Component {
   constructor(props) {
@@ -22,35 +20,40 @@ class App extends Component {
       selectedVideo: null
     };
     //set initial load state using the handler method
-    this.videoSearch(DEFAULT_SEARCH_TERM)
+    this.videoSearch(DEFAULT_SEARCH_TERM);
   }
 
   // handler methods - are defined outside constructors
   videoSearch(searchTerm) {
-// if search term is deleted, then set a default search term    
-    if (searchTerm === ""){
+    // if search term is deleted, then set a default search term
+    if (searchTerm === "") {
       return this.videoSearch(DEFAULT_SEARCH_TERM);
     }
-//search YT, retrieve videos, set state with videos array, choose initial video detail from index 0
-    YTSearch({ key: API_KEY, term: searchTerm }, (videos) => {
-      this.setState({ 
+    //search YT, retrieve videos, set state with videos array, choose initial video detail from index 0
+    YTSearch({ key: API_KEY, term: searchTerm }, videos => {
+      this.setState({
         videos: videos,
-        selectedVideo : videos[0]
-       }); 
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
-    const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 1000)
+    const self = this;
+    const cb = (term) => {
+      return this.videoSearch(term);
+    };
+    const videoSearch = _.debounce(cb, 300);
+    // const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 1000)
     return (
       <div>
-        <SearchBar onSearchTermChange={(searchTerm) => videoSearch(searchTerm)} />
+        <SearchBar onSearchTermChange={searchTerm => videoSearch(searchTerm)} />
         {/* <SearchBar onSearchTermChange={this.videoSearch(searchTerm)} /> */}
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList 
-        videos = {this.state.videos}
-        onVideoSelect={(selectedVideo)=>this.setState({selectedVideo})}  // func takes the clicked video as argument
-         />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })} // func takes the clicked video as argument
+        />
       </div>
     );
   }
