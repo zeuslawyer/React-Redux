5170574f8fa4fd54d0e0abd09253c3bd202685b5
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-19 09:45:50 +1100

    EXPERIMENT. refactor to put getlocation imported func. pass context as param. WORKING!

--- 3_seasons_state_components_lifecycle/src/index.js
+++ 3_seasons_state_components_lifecycle/src/index.js
@@ -1,33 +1,34 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import getLocation from './getLocation'
 
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {latitude: "fetching..."};
+    this.state = { latitude: null };
+    this.getLocation(this);
+    // console.log(this);
   }
+  
+  getLocation = getLocation;
+  // getLocation = () => {
+  //   window.navigator.geolocation.getCurrentPosition(
+  //     position => {
+  //       // console.log(position.coords.latitude.toString().slice(1,10));
+  //       this.setState({ latitude: position.coords.latitude });
+  //     },
+
+  //     err => console.log("ERROR in GEOLOCATION!  > > " + err.message)
+  //   );
+  // };
 
   render() {
-    console.log("STATE! ", this.state.latitude);
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // console.log(position.coords.latitude.toString().slice(1,10));
-        this.setState({
-          latitude: position.coords.latitude
-        })
-      },
-      err => console.log("ERROR in GEOLOCATION!  > > " + err.message)
-    );
-
-    return <div className="segment ui">Latitude: {this.state.latitude.toString().slice(1,10)} </div>;
+    
+    return <div className="segment ui"> Latitude: {this.state.latitude} </div>;
   }
 }
 
 export default App;
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-// const App = () => {
-
-// };
