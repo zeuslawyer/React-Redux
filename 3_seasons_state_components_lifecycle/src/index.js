f2760e74b9ea429e6fa79dd7168ac5c86bb8cd91
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-19 09:47:45 +1100

    remove experimental new getLocation.js file and undo refactor.

--- 3_seasons_state_components_lifecycle/src/index.js
+++ 3_seasons_state_components_lifecycle/src/index.js
@@ -1,27 +1,25 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-import getLocation from './getLocation'
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { latitude: null };
-    this.getLocation(this);
-    // console.log(this);
+    this.getLocation();
   }
   
-  getLocation = getLocation;
-  // getLocation = () => {
-  //   window.navigator.geolocation.getCurrentPosition(
-  //     position => {
-  //       // console.log(position.coords.latitude.toString().slice(1,10));
-  //       this.setState({ latitude: position.coords.latitude });
-  //     },
+  getLocation = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // console.log(position.coords.latitude.toString().slice(1,10));
+        this.setState({ latitude: position.coords.latitude });
+      },
 
-  //     err => console.log("ERROR in GEOLOCATION!  > > " + err.message)
-  //   );
-  // };
+      err => console.log("ERROR in GEOLOCATION!  > > " + err.message)
+    );
+  };
 
   render() {
     
