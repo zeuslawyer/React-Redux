a63096672327ab6971dc8b37d56aaf88ca2f55fa
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-20 14:17:50 +1100

    refactor code to use CONFIG OBJECT and DECONSTRUCTION. .. WORKING!

--- 3_seasons_state_components_lifecycle/src/SeasonDisplay.js
+++ 3_seasons_state_components_lifecycle/src/SeasonDisplay.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+const getSeason = latitude => {
+  let month = new Date().getMonth();
+
+  if (6 <= month <= 11) {
+    return latitude > 0 ? "Winter" : "Summer";
+  } else {
+    return latitude > 0 ? "Winter" : "Summer";
+  }
+};
+
+const seasonConfig = {
+  summer:{
+    displayText:"It's Summertime! Let's head to the beach, baby!",
+    iconName:"sun"
+  },
+  winter:{
+    displayText:"Rug up mate, it's chilly as!",
+    iconName:"snowflake"
+  }
+}
+
+
 const SeasonDisplay = props => {
-  let season = getSeason(props.latitude);
+  let season = getSeason(props.latitude).toLowerCase();
+  
 
   if (!season) {
     return (
@@ -13,26 +36,17 @@ const SeasonDisplay = props => {
     );
   }
 
-  let displayText =
-    season.toLowerCase() === "winter"
-      ? "Rug up mate, it's chilly as!"
-      : "It's Summertime! Let's head to the beach, baby!";
+  const {displayText, iconName} = seasonConfig[season]
 
   return (
-    <div>
-      <h1>{displayText}</h1>
+    <div >
+      <i className={`icon ${iconName} massive `}/>
+      <h1 className="ui center aligned header">{displayText}</h1>
+      <i className={`icon ${iconName} massive `}/>
     </div>
   );
 };
 
 export default SeasonDisplay;
 
-const getSeason = latitude => {
-  let month = new Date().getMonth();
 
-  if (6 <= month <= 11) {
-    return latitude > 0 ? "Winter" : "Summer";
-  } else {
-    return latitude > 0 ? "Winter" : "Summer";
-  }
-};
--- 3_seasons_state_components_lifecycle/src/loader.js
+++ 3_seasons_state_components_lifecycle/src/loader.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
+import React from "react";
+import ReactLoading from "react-loading";
 
-
-const Loader = ({type, color}) => (
-    <ReactLoading type='cylon' color='#ff0000' height={'10%'} width={'80%'} />
+const Loader = ({ type, color }) => (
+  <div className="ui segment align-right">
+    <ReactLoading type="cylon" color="#ff0000" height={"5%"} width={"80%"} />
+    {/* <i className="asterisk loading icon massive" /> */}
+  </div>
 );
- 
+
 export default Loader;
