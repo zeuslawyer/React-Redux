5660bb00246e165dcdc1402532ce9002aa500b1b
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-20 13:45:59 +1100

    linted season calculation logic on save. WORKING

--- 3_seasons_state_components_lifecycle/src/index.js
+++ 3_seasons_state_components_lifecycle/src/index.js
@@ -1,48 +1,46 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-import Loader from './loader'
-
+import Loader from "./loader";
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { latitude: null, errMessage:null };
-  // }
+  state = { latitude: null, errMessage: null };
 
-  state = { latitude: null, errMessage:null };
-
-  componentDidMount(){
-    console.log('component did mount! \n', this.state)
+  componentDidMount() {
     this.getLocation();
+    console.log("component did mount! State Object is: \n", this.state);
   }
-  componentDidUpdate(){
-    console.log('component did update! \n', this.state)
+  componentDidUpdate() {
+    console.log("component did update!State Object is: \n", this.state);
   }
 
   render() {
-    if(this.state.latitude && !this.state.errMessage) {
+    if (this.state.latitude && !this.state.errMessage) {
       return (
-        <div className="segment ui"> <SeasonDisplay latitude={this.state.latitude}/> </div>
-      ) 
+        <div className="segment ui">
+          {" "}
+          <SeasonDisplay latitude={this.state.latitude} />{" "}
+        </div>
+      );
     }
-    if (this.state.errMessage && !this.state.latitude ) {
-      return  <div className="segment ui"> Error: {this.state.errMessage} </div>
+    if (this.state.errMessage && !this.state.latitude) {
+      return <div className="segment ui"> Error: {this.state.errMessage} </div>;
     }
-    
+
     return (
-      <div className="center"> <Loader /></div> 
+      <div className="center">
+        <Loader />
+      </div>
     );
   }
 
   getLocation = () => {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ latitude: position.coords.latitude }),
-      err => this.setState({errMessage: err.message})
+      err => this.setState({ errMessage: err.message })
     );
   };
 }
-
 
 export default App;
 
