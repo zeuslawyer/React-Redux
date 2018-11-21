d1543d30a1adc8911558e44ce006166aa8d3ab1b
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-21 16:22:41 +1100

    EXPERIMENT.  implement a demo hitMeUp() call back for the onChange handler to show how to bind to this inside constructor. WORKING

--- 4_apis_pics_gallery/src/components/SearchBar.js
+++ 4_apis_pics_gallery/src/components/SearchBar.js
@@ -3,12 +3,21 @@ import React, { Component } from "react";
 class SearchBar extends Component {
   state = { searchTerm: "" };
 
+  constructor() {
+    super();
+    this.hitMeUp = this.hitMeUp.bind(this);
+  }
+
+  hitMeUp(event) {
+    console.log(this);
+    this.setState({ searchTerm: event.target.value })
+  }
   onFormSubmit(event) {
     event.preventDefault();
     console.log(this);
   }
   render() {
-    console.log(this.state.searchTerm)
+    console.log(this.state.searchTerm);
 
     return (
       <div className="ui segment">
@@ -19,9 +28,10 @@ class SearchBar extends Component {
               type="text"
               value={this.state.searchTerm}
               placeholder="Enter search text here"
-              onChange={event => {
-                this.setState({ searchTerm: event.target.value });
-              }}
+              //   onChange={event => {
+              //     this.setState({ searchTerm: event.target.value });
+              //   }}
+              onChange={this.hitMeUp}
             />
           </div>
         </form>
