db6989dba6450a8d716b43d729fcc6660f3c888a
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-21 12:02:10 +1100

    add some styling from semantic ui

--- 4_apis_pics_gallery/src/components/App.js
+++ 4_apis_pics_gallery/src/components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import SearchBar from "./SearchBar";
+
+const style = {marginTop:"10px"}
 
 function App() {
-  return <div>"Hello, world!"</div>;
+  return (
+    <div className="ui container" style={style}>
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
