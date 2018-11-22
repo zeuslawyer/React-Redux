794d4461da356bcb8367cec35edcefa90d8dfe48
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-22 16:31:21 +1100

    revert to .then syntax for promises. implement setState() using API response object with conditional rendering.WORKING

--- 4_apis_pics_gallery/src/components/App.js
+++ 4_apis_pics_gallery/src/components/App.js
@@ -6,25 +6,31 @@ import SearchBar from "./SearchBar";
 const componentStyle = { marginTop: "10px" };
 
 class App extends Component {
-  handleResponse = (response) =>{
-    console.log(response.data.results)
-  }
-  onSearchSubmit = async term => {
-    const response = await axios
+  state = { images: [] };
+  handleResponse = response => {
+    console.log('handling response....');
+    this.setState({ images: response.data.results });
+  };
+  onSearchSubmit = term => {
+    axios
       .get("https://api.unsplash.com/search/photos", {
         params: { query: term },
         headers: {
           Authorization: "Client-ID " + process.env.REACT_APP_UNSPLASH_ACCESS
         }
       })
-      // .then(d => console.log(d.data.results[1].urls.regular))
-      // .then(this.handleResponse)
-      // .catch(e => console.log(e.message));
-
-      console.log(response.data.results[0].urls.regular);
+      .then(this.handleResponse)
+      .catch(e => console.log(e.message));
   };
 
   render() {
+    console.log('Number of images in array....', this.state.images.length);
+    if (this.state.images.length > 0) {
+      return (
+        <div>There are {this.state.images.length} images available...</div>
+      )
+    }
+
     return (
       <div className="ui container" style={componentStyle}>
         <SearchBar onFormSubmit={this.onSearchSubmit} />
