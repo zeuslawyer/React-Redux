e587d4af59ae4bb8670b5208e99a109d26eff5ec
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-23 13:40:00 +1100

    temp commit on dynamic spans.  NOT WORKING RIGHT

--- 4_apis_pics_gallery/src/components/ImageCard.js
+++ 4_apis_pics_gallery/src/components/ImageCard.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { span: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(
+      this.imageRef.current.height === this.imageRef.current.clientHeight
+    );
+
+    //listen for the imageRef.current object to full load, then calculate spans using callback
+    this.imageRef.current.addEventListener("load", this.setImageSpans);
+  }
+
+  setImageSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const gridRowHeight = 10; //taken from the ImageList css grid-auto-rows rule
+    const span = Math.ceil(height / 10);
+    this.setState({ span });
+  };
+
+  componentDidUpdate() {
+    // console.log("image card state re set and re rendered");
+  }
+
   render() {
-    let { urls, description } = this.props.image;
+    const { urls, description } = this.props.image;
 
     return (
-      <div>
-        <img src={urls.regular} alt={description} />
+      <div style={{ gridRowEnd: `span $(this.state.span)` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
--- 4_apis_pics_gallery/src/components/ImageList.js
+++ 4_apis_pics_gallery/src/components/ImageList.js
@@ -3,12 +3,12 @@ import ImageCard from "./ImageCard";
 import "./ImageList.css";
 
 const ImageList = props => {
-  //map the array of images to create an array of JSX elements
+  //MAP the array of images to create an array of JSX elements
   const images = props.images.map(image => {
     return <ImageCard className="image-card" key={image.id} image={image} />;
   });
 
-  // render the array of JSX elements
+  // RENDER the array of JSX elements
   return <div className="image-list">{images}</div>;
 };
 
