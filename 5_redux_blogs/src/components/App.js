4c6cbf1031888156d58a34ac6d51ea273b45cbf0
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-26 11:11:36 +1100

    initial app set up with basic redux related config. Working

---
+++ 5_redux_blogs/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="ui container">
+      ...the App...
+    </div>
+  )
+}
+
+export default App;
---
+++ 5_redux_blogs/src/reducers/index.js
@@ -0,0 +1,6 @@
+import actions from '../actions/index.js'
+import { combineReducers } from 'redux';
+
+
+
+ export default combineReducers({})
