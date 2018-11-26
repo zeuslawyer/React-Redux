8eace48bbec5c73509e7064b5093e28a29d00e7a
zubin aka zeuscoder <zubin.pratap@gmail.com>
2018-11-26 12:43:06 +1100

    replicate redux/axios error message from lecture 156

--- 5_redux_blogs/src/actions/index.js
+++ 5_redux_blogs/src/actions/index.js
@@ -1,9 +1,22 @@
-const dummyAction = () => {
-    console.log('dummy action just ran');
-    return {
-        type:"DUMMY_ACTION",
-        payload: {name:'dummy action'}
-    }
-}
+import jsonPlaceholder from "../apis/jsonplaceholder";
 
-export {dummyAction};
+const dummyAction = () => {
+  jsonPlaceholder
+    .get("/posts")
+    .then(res => {
+        return{
+            type: "DUMMY_ACTION",
+            payload: {name: 'dummy name'}
+        }
+    })
+    .catch(e => console.log(e.message));
+
+  console.log("axios fetch action just ran");
+
+  // return {
+  //     type:"DUMMY_ACTION",
+  //     payload: {name:'dummy action'}
+  // }
+};
+
+export { dummyAction };
